e905af1c0a87f629be1bc35421ef73cfb0de4fb0
sssv587 <1092663295@qq.com>
2021-02-22 23:05:04 +0800

    InnerClass

--- src/com/futurebytedance/day06/innerclass/ScalaInnerClassDemo.scala
+++ src/com/futurebytedance/day06/innerclass/ScalaInnerClassDemo.scala
@@ -7,7 +7,10 @@ package com.futurebytedance.day06.innerclass
  * @Description Scala内部类的使用
  * 内部类访问外部类属性
  * 1)内部类如果想要访问外部类的属性，可以通过外部类对象访问。即：访问方式：外部类名.this.属性名
- * 2)内部类如果想要访问外部类的属性，也可以通过外部类别名访问(推荐)。即：访问方式：外部类名别名.属性名   【外部类名.this  等价 外部类名别名】
+ * 2)内部类如果想要访问外部类的属性，也可以通过外部类别名访问(推荐)。即：访问方式：外部类名别名.属性名【外部类名.this  等价 外部类名别名】
+ *
+ * 类型投影
+ * 1)类型投影是指：在方法声明上，如果使用  外部类#内部类  的方式，表示忽略内部类的对象关系，等同于Java中内部类的语法操作，我们将这种方式称之为 类型投影（即：忽略对象的创建方式，只考虑类型）
  */
 object ScalaInnerClassDemo {
   def main(args: Array[String]): Unit = {
@@ -21,6 +24,13 @@ object ScalaInnerClassDemo {
 
     //使用inner1调用info()
     inner1.info()
+
+    //去调用test
+    inner1.test(inner1)
+    inner1.test(inner2)
+
+    //在默认情况下，scala的内部类的实例和创建内部类实例的外部对象关联。
+
 
     //创建静态内部类对象
     val staticInner = new ScalaOuterClass.ScalaStaticInnerClass()
@@ -57,6 +67,12 @@ class ScalaOuterClass {
       println("-----------------------------------")
       println("name = " + myOuter.name
         + " sal =" + myOuter.sal)
+    }
+
+    //这里有一个方法，可以接受ScalaInnerClass实例
+    //下面的ScalaOuterClass#ScalaInnerClass类型投影的作用就是屏蔽外部对象对内部类对象的影响
+    def test(ic: ScalaOuterClass#ScalaInnerClass): Unit = {
+      println("使用了类型投影:" + ic)
     }
   }
 
