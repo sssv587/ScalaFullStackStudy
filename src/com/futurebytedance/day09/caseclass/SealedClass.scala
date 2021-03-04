32758c38ef0035445391265a68799353728db4f5
sssv587 <1092663295@qq.com>
2021-03-04 23:03:29 +0800

    SealedClass

---
+++ src/com/futurebytedance/day09/caseclass/SealedClass.scala
@@ -0,0 +1,17 @@
+package com.futurebytedance.day09.caseclass
+
+/**
+ * @author yuhang.sun
+ * @date 2021/3/4 - 23:01
+ * @version 1.0
+ * @Description 密封类
+ * 1)如果想让case类的所有子类都必须在申明该类的相同的源文件中定义，可以将样例类的通用超类声明为sealed，这个超类称之为密封类。
+ * 2)密封就是不能在其他文件中定义子类。
+ */
+object SealedClass {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
+
+//class AAA extends Item
